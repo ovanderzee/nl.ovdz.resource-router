0229b30f6c01a54c19cdc2c094d1d19051ab3115
Onno van der Zee <ovanderzee@iprofs.nl>
2016-05-19 17:21:47 +0200

    return to cli when lint fails initiallly, stay when lint fails within watch

--- gulpfile.js
+++ gulpfile.js
@@ -12,36 +12,47 @@ var paths = {
     styles: [webroot + '/**/*.css', extension + '/**/*.css'],
 };
 
-var lintJSObjects = function(event) {
-    return gulp.src(event.path)
+var lintJSObjects = function(event, failOnError) {
+    var result = gulp.src(event.path)
         .pipe(jsonlint())
         .pipe(jsonlint.reporter());
+    if (failOnError) {
+        result.pipe(jsonlint.failOnError());
+    }
+    return result;
 };
 
-var lintScripts = function(event) {
-    return gulp.src(event.path)
+var lintScripts = function(event, failOnError) {
+    var result = gulp.src(event.path)
         .pipe(jshint())
-        .pipe(jshint.reporter('default'))
-        .pipe(jshint.reporter('fail'))
+        .pipe(jshint.reporter())
         .pipe(jscs())
         .pipe(jscs.reporter());
+    if (failOnError) {
+        result.pipe(jshint.reporter('fail'))
+    }
+    return result;
 };
 
-var lintStyles = function(event) {
-    return gulp.src(event.path)
+var lintStyles = function(event, failOnError) {
+    var result = gulp.src(event.path)
         .pipe(csslint())
         .pipe(csslint.reporter());
+    if (failOnError) {
+        result.pipe(csslint.reporter('fail'));
+    }
+    return result;
 };
 
 gulp.task('lint', function() {
     for (var i = 0; i < paths.scripts.length; i++) {
-        lintJSObjects({path: paths.jsobjects[i]});
+        lintJSObjects({path: paths.jsobjects[i]}, true);
     }
     for (var i = 0; i < paths.scripts.length; i++) {
-        lintScripts({path: paths.scripts[i]});
+        lintScripts({path: paths.scripts[i]}, true);
     }
     for (var i = 0; i < paths.scripts.length; i++) {
-        lintStyles({path: paths.styles[i]});
+        lintStyles({path: paths.styles[i]}, true);
     }
     gulp.watch(paths.jsobjects, lintJSObjects);
     gulp.watch(paths.scripts, lintScripts);
