bbf9b4470c8a64c2902848f5d0f9a9dd3769e585
Onno van der Zee <onno.van.der.zee@finalist.nl>
2016-12-30 15:39:00 +0100

    set proper badge color initially

--- extension/background.js
+++ extension/background.js
@@ -1,23 +1,10 @@
 // init badge decoration
 
 extensionModel.init();
-
-var activeCount = 0;
-for (var i = 0; i < extensionModel.urls.length; i++) {
-    var item = JSON.parse(localStorage.getItem(extensionModel.urls[i]));
-    if (Boolean(item.active)) {
-        activeCount++;
-    }
-}
-chrome.browserAction.setBadgeText({text: String(activeCount)});
-
-chrome.browserAction.setBadgeBackgroundColor({color: [0, 0, 0, 255]});
-
-
-
+extensionModel.setBadge();
 
 var routingResponse = function (details) {
-    if (!localStorage.running || urlModel.isValidating(details.url)) {
+    if (!extensionModel.isRunning || urlModel.isValidating(details.url)) {
 		return {cancel: false};
     }
     var localRsrc = routeModel.localRsrc(details.url);
--- extension/extension-model.js
+++ extension/extension-model.js
@@ -22,6 +22,18 @@ var extensionModel = new function () {
             this.urls.push(key);
         }
     }
+    var activeState = {
+        className: ' active',
+        color: [50, 205, 50, 255],
+        description: 'engaged',
+        toggleHint: 'dismiss'
+    }
+    var passiveState = {
+        className: ' passive',
+        color: [255, 165, 0, 255],
+        description: 'dismissed',
+        toggleHint: 'engage'
+    }
 
     this.init = function () {
         // merge localStorage in settings
@@ -36,6 +48,13 @@ var extensionModel = new function () {
         localStorage.setItem(this.id, this.value);
     };
 
+    this.isRunning = function () {
+        return self.settings.running === 'running';
+    };
+    this.stateVars = function () {
+        return self.isRunning() ? activeState : passiveState;
+    };
+
     this.activate = function () {
 		self.set.call({id: 'running', value: 'running'});
 		self.activateView.call(this.form);
@@ -47,12 +66,24 @@ var extensionModel = new function () {
 
     this.activateView = function () {
         this.className = this.className.replace(' passive', '');
-        this.className += ' active';
-        chrome.browserAction.setBadgeBackgroundColor({color: [50, 205, 50, 255]});
+        this.className += activeState.className;
+        chrome.browserAction.setBadgeBackgroundColor({color: activeState.color});
     };
     this.deactivateView = function () {
         this.className = this.className.replace(' active', '');
-        this.className += ' passive';
-        chrome.browserAction.setBadgeBackgroundColor({color: [255, 165, 0, 255]});
+        this.className += passiveState.className;
+        chrome.browserAction.setBadgeBackgroundColor({color: passiveState.color});
+    };
+
+    this.setBadge = function () {
+        var activeCount = 0;
+        for (var i = 0; i < self.urls.length; i++) {
+            var item = JSON.parse(localStorage.getItem(self.urls[i]));
+            if (Boolean(item.active)) {
+                activeCount++;
+            }
+        }
+        chrome.browserAction.setBadgeText({text: String(activeCount)});
+        chrome.browserAction.setBadgeBackgroundColor({color: extensionModel.stateVars().color});
     };
 };
