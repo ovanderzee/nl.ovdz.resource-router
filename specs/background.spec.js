904ccb54e94700111f7eb1edb714581e1f0dc680
Onno van der Zee <ovanderzee@iprofs.nlhastalavista@dds.nl>
2017-06-26 01:10:50 +0200

    added mocha hooks

--- specs/background.spec.js
+++ specs/background.spec.js
@@ -1,15 +1,26 @@
 
-localStorage.setItem ('http://www.test.it/css/active.css', '{"active":true,"local":"test.it/styles.css"}');
-localStorage.setItem ('http://www.test.it/css/passive.css', '{"active":false,"local":"test.it/styles.css"}');
-localStorage.setItem ('validating', '{}');
-localStorage.setItem ('rerouting', 'running');
-
 describe('Extension Background: conditional redirection', function () {
 
+    var details;
+    var setData = function () {
+	    details = {url: 'http://www.test.it/css/active.css'};
+        localStorage.setItem ('http://www.test.it/css/active.css', '{"active":true,"local":"test.it/styles.css"}');
+        localStorage.setItem ('http://www.test.it/css/passive.css', '{"active":false,"local":"test.it/styles.css"}');
+        localStorage.setItem ('loose', 'localhost:9080');
+        localStorage.setItem ('rerouting', 'running');
+        localStorage.setItem ('secure', 'localhost:9443');
+        localStorage.setItem ('validating', '{}');
+    };
+
+    // runs before on start of this block
+    before(setData);
+
+    // runs after each test in this block
+    afterEach(setData);
+
 	it('should redirect when all conditions are set', function () {
-	    var details = {url: 'http://www.test.it/css/active.css'};
         var redirection = routingResponse(details);
-        expect(redirection.redirectUrl).to.equal("http://localhost:80/test.it/styles.css");
+        expect(redirection.redirectUrl).to.equal("http://localhost:9080/test.it/styles.css");
 	});
 
 	it('should not redirect a unregistered route', function () {
@@ -25,20 +36,16 @@ describe('Extension Background: conditional redirection', function () {
 	});
 
 	it('should not redirect when the extention is dismissed', function () {
-	    var details = {url: 'http://www.test.it/css/active.css'};
 	    localStorage.setItem ('rerouting', 'idle');
         var redirection = routingResponse(details);
         expect(redirection.cancel).to.be.false;
-	    localStorage.setItem ('rerouting', 'running');
 	});
 
 	it('should not redirect when the url is being validated', function () {
-	    var details = {url: 'http://www.test.it/css/active.css'};
 	    var validationObject = {'http://www.test.it/css/active.css': '#route_1 input[name="live"]'};
 	    localStorage.setItem ('validating', JSON.stringify(validationObject));
         var redirection = routingResponse(details);
         expect(redirection.cancel).to.be.false;
-	    localStorage.setItem ('validating', {});
 	});
 
 });
