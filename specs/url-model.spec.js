740b50972148c4c633009f915694f7e5be75ca96
Onno van der Zee <ovanderzee@iprofs.nlhastalavista@dds.nl>
2017-07-10 10:13:07 +0200

    test url model

--- specs/url-model.spec.js
+++ specs/url-model.spec.js
@@ -1,15 +1,31 @@
 
 describe('URL Model: validate entered URL\'s', function () {
 
-	it('should confirm the validation mode', function () {
-	    localStorage.setItem('validating', true);
-	    var validationMode = urlModel.stateVars();
+    // executing localStorage.clear in a before hook will raise an error while local servers a tested
+    localStorage.clear();
+
+    var liveUrl = "http://www.bv.nl/test-url";
+    var liveElementQuery = "route_1 input[name=\"live\"]";
+    var localUrl = "/local-rsrc";
+    var localElementQuery = "route_1 input[name=\"local\"]";
+    var testStack = {};
+    testStack[liveUrl] = liveElementQuery;
+    testStack[localUrl] = localElementQuery;
+
+	it('should initiate an empty validation stack', function () {
+	    var validationStack = localStorage.getItem('validating');
+        expect(validationStack === '{}').to.be.true;
+	});
+
+	it('should confirm the validation mode for a specific URL', function () {
+	    localStorage.setItem('validating', JSON.stringify(testStack));
+	    var validationMode = urlModel.isValidating(liveUrl);
         expect(validationMode).to.be.true;
 	});
 
-	it('should negate the validation mode', function () {
-	    localStorage.setItem('validating', false);
-	    var validationMode = urlModel.stateVars();
+	it('should negate the validation mode for a specific URL', function () {
+	    localStorage.setItem('validating', '{}');
+	    var validationMode = urlModel.isValidating(liveUrl);
         expect(validationMode).to.be.false;
 	});
 
